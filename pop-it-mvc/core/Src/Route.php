70b9b05c057115bbaf5a55f074980ee8f664df5b
Dima <dimahudobin05@gmail.com>
2023-03-31 08:35:23 +0300

    Использование illuminate/database и illuminate/events

--- pop-it-mvc/core/Src/Route.php
+++ pop-it-mvc/core/Src/Route.php
@@ -24,7 +24,6 @@ class Route
     {
         $path = explode('?', $_SERVER['REQUEST_URI'])[0];
         $path = substr($path, strlen(self::$prefix) + 1);
-
         if (!array_key_exists($path, self::$routes)) {
             throw new Error('This path does not exist');
         }
